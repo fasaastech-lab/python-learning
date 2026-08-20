25c9646f87764d824c7d267d49a50784643a2ed3
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-08-20 17:29:40 +0100

    Simplify travel weather logic

--- freeCodeCamp/travel_weather_planner.py
+++ freeCodeCamp/travel_weather_planner.py
@@ -1,27 +1,15 @@
 distance_mi = 4
+
 is_raining = False
 has_bike = True
 has_car = False
 has_ride_share_app = False
 
-
 if not distance_mi:
     print(False)
-
 elif distance_mi <= 1:
-    if not is_raining:
-        print(True)
-    else:
-        print(False)
-
+    print(not is_raining)
 elif distance_mi <= 6:
-    if has_bike and not is_raining:
-        print(True)
-    else:
-        print(False)
-
+    print(has_bike and not is_raining)
 else:
-    if has_car or has_ride_share_app:
-        print(True)
-    else:
-        print(False)
+    print(has_car or has_ride_share_app)
